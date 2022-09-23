1740d5765b92212ae40da9c742998af37e1825dc
Oliver Praesto <oliver.praesto@gmail.com>
2022-09-23 16:10:20 +0200

    - adds mock for handleEdit

--- src/lib/components/MovieList.svelte
+++ src/lib/components/MovieList.svelte
@@ -3,12 +3,18 @@
 	import type { Movie } from '$lib/types';
 
 	export let movies: Movie[] = [];
+
+	const handleEdit = (movie: Movie) => {
+		// TODO: Create edit handler for form. Use a store? EventDispatcher?
+		console.log(movie);
+	};
 </script>
 
 <div class="overflow-x-auto">
 	<table class="table w-full">
 		<thead>
 			<tr>
+				<th>&nbsp;</th>
 				<th>Title</th>
 				<th>Director</th>
 				<th>Released</th>
@@ -18,6 +24,12 @@
 		<tbody>
 			{#each movies as { title, director, year, _id }}
 				<tr>
+					<td class="w-1">
+						<button
+							on:click={() => handleEdit({ title, director, year })}
+							class="btn btn-ghost btn-sm btn-primary">🖋️</button
+						>
+					</td>
 					<td>{title}</td>
 					<td>{director}</td>
 					<td>{year}</td>
