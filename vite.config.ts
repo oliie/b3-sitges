662d3a4cd87095aa0a848850a93acd4d5142d315
Oliver Praesto <oliver.praesto@gmail.com>
2022-09-30 09:03:35 +0200

    - updates vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,7 @@ import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
 	plugins: [sveltekit()],
-	build: {
-		minify: true
-	}
+	assetsInclude: './static/output.css'
 };
 
 export default config;
